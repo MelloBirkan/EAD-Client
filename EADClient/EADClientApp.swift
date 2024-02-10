4dab18b39302cc476fafea78492c9b1096ede325
Mello Birkan <mgbirkan@icloud.com>
2024-02-10 01:17:11 -0300

    Initial Commit

---
+++ EADClient/EADClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  EADClientApp.swift
+//  EADClient
+//
+//  Created by Marcello Gonzatto Birkan on 10/02/24.
+//
+
+import SwiftUI
+
+@main
+struct EADClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
