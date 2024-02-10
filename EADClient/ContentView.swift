4025e4c0dada3869c70c7bf47fb86b377bc44916
Mello Birkan <mgbirkan@icloud.com>
2024-02-10 02:14:37 -0300

    Componente dos cards adicionados

--- EADClient/ContentView.swift
+++ EADClient/ContentView.swift
@@ -9,16 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    HStack {
+    ZStack {
+      // MARK: Background Card
       VStack {
-        Text("UI Design")
-          .font(.title)
-          .fontWeight(.semibold)
-        Text("Certificate")
-        .foregroundColor(Color("AccentColor"))
+        Spacer()
       }
-      Spacer()
-      Image("Logo1")
+      .frame(width: 300, height: 220)
+      .background(Color.blue)
+      .cornerRadius(20)
+      .shadow(radius: 20)
+      .offset(x:0, y: -20)
+      
+      // MARK: Front Card
+      VStack {
+        HStack {
+          VStack {
+            Text("UI Design")
+              .font(.title)
+              .fontWeight(.semibold)
+              .foregroundStyle(.white)
+            Text("Certificate")
+            .foregroundColor(Color("AccentColor"))
+          }
+          Spacer()
+          Image("Logo1")
+        }
+        .padding(.horizontal, 20)
+        .padding(.top, 20)
+        Spacer()
+        Image("Card1")
+          .resizable()
+          .aspectRatio(contentMode: .fill)
+          .frame(width: 300, height: 110, alignment: .top)
+      }
+      .frame(width: 340, height: 220)
+      .background(Color.black)
+      .cornerRadius(20)
+    .shadow(radius: 20)
     }
   }
 }
