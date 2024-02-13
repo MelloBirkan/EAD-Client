450b66f7f7fc405fcbfbc3073ecf355baa27eca9
Mello Birkan <mgbirkan@icloud.com>
2024-02-13 02:40:06 -0300

    Animações adicionadas no profile na home.

--- Home.swift
+++ Home.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct Home: View {
   @State private var showProfile = false
+  @State private var viewState = CGSize.zero
   
   var body: some View {
     ZStack {
@@ -42,14 +43,31 @@ struct Home: View {
       .shadow(color: .black.opacity(0.2), radius: 20, x: 0, y: 20)
       .offset(y: showProfile ? -450 : 0)
       .rotation3DEffect(
-        .degrees(showProfile ? -10 : 0), axis: (x: 10.0, y: 0.0, z: 0.0))
+        .degrees(showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10.0, y: 0.0, z: 0.0))
       .scaleEffect(showProfile ? 0.90 : 1)
-      .animation(.spring(response: 0.5, dampingFraction: 0.6))
+      .animation(.spring(response: 0.5, dampingFraction: 0.6), value: showProfile)
       .ignoresSafeArea()
       
       MenuView()
-        .offset(y: showProfile ? 0 : 600)
-        .animation(.spring(response: 0.5, dampingFraction: 0.6))
+        .background(Color.black.opacity(0.001))
+        .offset(y: showProfile ? 0 : 1000)
+        .offset(y: viewState.height)
+        .animation(.spring(response: 0.5, dampingFraction: 0.6), value: showProfile)
+        .onTapGesture {
+          showProfile.toggle()
+          
+        }
+        .gesture(
+          DragGesture().onChanged { value in
+            viewState = value.translation
+          }
+            .onEnded{ value in
+              if viewState.height > 50 {
+                showProfile = false
+              }
+              viewState = .zero
+            }
+        )
     }
   }
 }
